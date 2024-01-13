97acc9245a566570d3596bac7894cf7590d01821
Laurence Kuo <lakuo@ucsd.edu>
2024-01-12 18:29:13 -0800

    update

--- server/server.js
+++ server/server.js
@@ -111,6 +111,15 @@ app.post("/api/mark-used", async (req, res) => {
   }
 });
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "../frontend/build/index.html"));
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
