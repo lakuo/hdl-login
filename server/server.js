17921ddc9434e5718fbe82e5535b28a79fc3453d
Laurence Kuo <lakuo@ucsd.edu>
2024-01-12 19:10:05 -0800

    change

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { google } = require("googleapis");
 const cors = require("cors");
-
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
 
